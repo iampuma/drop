d26c5d085363562d3217a79a23e95f5ec71ebb31
Nicky Vandevoorde <nicky@Nickys-MacBook.local>
2015-11-30 22:43:04 +0100

    Fixed d7 references in script

--- scripts/d8.sh
+++ scripts/d8.sh
@@ -1,8 +1,5 @@
 # Install latest drush cli to be able to install Drupal 8.
 composer global require drush/drush:dev-master
-# Get drupal 7.x and install.
+# Get drupal 8.x and install.
 sudo drush dl drupal-8 -y --destination=/usr/share/nginx --drupal-project-rename=htdocs
 cd /usr/share/nginx/htdocs && sudo drush si standard -y --account-name=admin --account-pass=admin --db-url=mysql://admin:admin@localhost/admin --db-su=root --site-name="Drop"
-# Replace the default vhost with our own.
-sudo rm /etc/nginx/sites-available/default
-sudo ln -s /usr/share/nginx/init/default /etc/nginx/sites-available/default
